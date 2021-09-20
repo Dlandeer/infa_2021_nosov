3de2ba82984f26d69f202e7575ff818710e9abbc
Dlandeer <dlander.danil@ya.ru>
2021-09-20 17:50:56 +0300

    Smile

---
+++ picture1.py
@@ -0,0 +1,16 @@
+from graph import *
+windowSize(400,400)
+brushColor("yellow")
+circle(200,200,100)
+brushColor("red")
+circle(150,175,30)
+circle(250,175,20)
+brushColor("black")
+circle(150,175,10)
+circle(250,175,10)
+p=[(115,85),(125,75),(185,140),(175,150)]
+polygon(p)
+po=[(315,75),(325,85),(235,150),(225,140)]
+polygon(po)
+rectangle(150,275,250,245)
+run()
